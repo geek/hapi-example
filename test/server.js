9e0c26342bf0301689e00d1b72f9b378b778b8fc
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-11-21 11:34:27 +0100

    Minimal Lab test setup

---
+++ test/server.js
@@ -0,0 +1,13 @@
+const Lab = require('lab');
+const Code = require('code');
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+describe('Example Server', function() {
+
+ it('is pending');
+
+});
